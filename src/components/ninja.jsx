ea1e1692dfb65353925609443b44a3eedd9d1043
ndemeyvan <ndemeyvan@gmail.com>
2021-01-08 18:19:18 +0100

    create a list with react js

--- src/components/ninja.jsx
+++ src/components/ninja.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Ninja extends Component {
-  render() {
-    const {  ninjas } = this.props;
-    //generate a new list of element with the object get from
-    //parent props
-    const ninjaList = ninjas.map((ninja) => {
-      return (
-        <div className="ninjaList" key={ninja.id}>
-          <div>Name : {ninja.name}</div>
-          <div>Age : {ninja.age}</div>
-          <div>Belt : {ninja.belt}</div>
-          <br />
-        </div>
-      );
-    });
+const ninja = (props) => {
+  const { ninjas } = props; /// destructuring method
+  //generate a new list of element with the object get from
+  //parent props
+  const ninjaList = ninjas.map((ninja) => {
     return (
-      <div className="ninja">
-        {/* Output a list  */}
-        {/* Utiliser les maps */}
-        {ninjaList}
+      <div className="ninjaList" key={ninja.id}>
+        <div>Name : {ninja.name}</div>
+        <div>Age : {ninja.age}</div>
+        <div>Belt : {ninja.belt}</div>
+        <br />
       </div>
     );
-  }
-}
+  });
+
+  //////////screen
+  return (
+    <div className="ninja">
+      {/* Output a list  */}
+      {/* Utiliser les maps */}
+      {ninjaList}
+    </div>
+  );
+};
+
+export default ninja;
